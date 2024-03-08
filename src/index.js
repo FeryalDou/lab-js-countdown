ccaf23df0f8c30d3e761b6f7e8d93539c975c83d
Feryal Doubali <feryalhalima@gmail.com>
2024-03-08 15:48:09 +0100

    correct in class

--- src/index.js
+++ src/index.js
@@ -1,38 +1,69 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
-let timer = null; // Variable to store the interval
-
-
+let intervalId = null; // Variable to store the interval
+let timeoutId = null;
+const startButton = document.getElementById("start-btn");
+const timeElement = document.getElementById("time");
+const toast = document.getElementById("toast");
+const toastMessage = document.getElementById("toast-message");
+const closeToastElement = document.getElementById("close-toast");
 
 // ITERATION 1: Add event listener to the start button
-
-// Your code goes here ...
-
-
-
+//const startButton = document.querySelector("#start-btn");
+startButton.addEventListener("click", startCountdown);
+toast.classList.remove("show");
+clearTimeout(timeoutId);
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
-
-  // Your code goes here ...
+  startButton.hidden = true;
+  intervalId = setInterval(() => {
+    timeElement.textContent = remainingTime;
+    if (remainingTime === 10) {
+      showToast("Final countdown");
+    }
+    if (remainingTime === 5) {
+      showToast("rev up those engines");
+    }
+    if (remainingTime === 0) {
+      showToast("lift off");
+      return;
+    }
+    remainingTime--;
+  }, 1000);
 }
 
-
-
-
 // ITERATION 3: Show Toast
+
 function showToast(message) {
   console.log("showToast called!");
+  toast.classList.add("show");
+  toastMessage.textContent = message;
+  timeOutId = setTimeout(() => {
+    toast.classeList.remove();
+    //const toast = document.createElement("div");
+    //document.body.appendChild(toast);
+  }, 3000);
+}
 
-  // Your code goes here ...
+// BONUS: ITERATION 4: TOAST CLOSE BUTTON
+function showToastWithCloseButton(message) {
+  console.log("showToastWithCloseButton called!");
 
+  toast = document.createElement("div");
+  toast.id = "toast";
+  toast.textContent = message;
+  toast.classList.add("toast");
 
+  const closeButton = document.createElement("button");
+  closeButton.textContent = "Close";
+  closeButton.classList.add("close-toast");
 
+  closeButton.addEventListener("click", () => {
+    toast.remove();
+  });
 
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+  toast.appendChild(closeButton);
 }
